d2eaca4b18f09561edf576a940d2c8abc405e5cf
Igor Santos <falaigors@gmail.com>
2022-08-31 00:24:33 -0300

    create Summary component

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,3 +1,32 @@
+import { SummaryCard, SummaryContainer } from "./styled";
+import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
+
 export function Summary() {
-  return <div>Summary</div>;
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={23} color="#00b37e" />
+        </header>
+        <strong> R$ 17.400,00</strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={23} color="#F75A68" />
+        </header>
+        <strong> R$ 17.400,00</strong>
+      </SummaryCard>
+
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={23} color="#fff" />
+        </header>
+        <strong> R$ 17.400,00</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  );
 }
--- src/components/Summary/styled.ts
+++ src/components/Summary/styled.ts
@@ -1,34 +1,43 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const HeaderContainer = styled.header`
-  background: ${(props) => props.theme["gray-900"]};
-  padding: 2.5rem 0 7.5rem;
-`;
-
-export const HeaderContent = styled.div`
+export const SummaryContainer = styled.section`
   width: 100%;
   max-width: 1120px;
   margin: 0 auto;
   padding: 0 1.5rem;
 
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+
+  margin-top: -5rem;
 `;
 
-export const NewTransactionButton = styled.button`
-  height: 50px;
-  border: 0;
+interface SummaryCardProps {
+  variant?: "green";
+}
+
+export const SummaryCard = styled.div<SummaryCardProps>`
+  background-color: ${(props) => props.theme["gray-600"]};
   border-radius: 6px;
-  color: ${(props) => props.theme["white"]};
-  background: ${(props) => props.theme["green-500"]};
+  padding: 2rem;
 
-  padding: 0 1.25rem;
-  font-weight: bold;
-  cursor: pointer;
-
-  &:hover {
-    transition: background-color 0.2s;
-    background: ${(props) => props.theme["green-700"]};
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: ${(props) => props.theme["gray-300"]};
   }
+
+  strong {
+    display: block;
+    margin-top: 1rem;
+    font-size: 2rem;
+  }
+
+  ${(props) =>
+    props.variant === "green" &&
+    css`
+      background-color: ${props.theme["green-700"]};
+    `}
 `;
