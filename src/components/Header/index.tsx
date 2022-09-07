62b68cf35967267b515a158d9c902177db7ee11c
Igor Santos <falaigors@gmail.com>
2022-09-07 19:59:19 -0300

    Estrutura visual - tabela de transações

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ export function Header() {
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransactionButton>Nova Trnasação</NewTransactionButton>
+        <NewTransactionButton>Nova Transação</NewTransactionButton>
       </HeaderContent>
     </HeaderContainer>
   );
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,40 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styled";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Categoria</td>
+              <td>07/09/2022</td>
+            </tr>
+
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 100,00</PriceHighlight>
+              </td>
+              <td>Alimentacao</td>
+              <td>07/09/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
