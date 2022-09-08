f9975600597f3609289eadc6823b63838269f4ed
Igor Santos <falaigors@gmail.com>
2022-09-07 21:13:28 -0300

    Estrutura visual - Criando radio button acessivel

--- src/components/NewTransactionModal/styles.tsx
+++ src/components/NewTransactionModal/styles.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import * as Dialog from "@radix-ui/react-dialog";
+import { Item, Root } from "@radix-ui/react-radio-group";
 
 export const Overlay = styled(Dialog.Overlay)`
   position: fixed;
@@ -71,7 +72,7 @@ export const Content = styled(Dialog.Content)`
   }
 `;
 
-export const TransactionType = styled.div`
+export const TransactionType = styled(Root)`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 1rem;
@@ -82,7 +83,7 @@ interface TransactionTypeButtonProps {
   variant: "income" | "outcome";
 }
 
-export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+export const TransactionTypeButton = styled(Item)<TransactionTypeButtonProps>`
   background: ${(props) => props.theme["gray-700"]};
   padding: 1rem;
   display: flex;
@@ -97,14 +98,11 @@ export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
   &:hover {
     background: ${(props) =>
       props.variant === "income"
-        ? props.theme["green-300"]
-        : props.theme["red-300"]};
+        ? props.theme["green-500"]
+        : props.theme["red-500"]};
     transition: background-color 0.2s;
 
     color: ${(props) => props.theme.white};
-    svg {
-      color: ${(props) => props.theme.white};
-    }
   }
 
   svg {
@@ -112,5 +110,21 @@ export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
       props.variant === "income"
         ? props.theme["green-300"]
         : props.theme["red-300"]};
+  }
+
+  &[data-state="unchecked"]:hover {
+    background: ${(props) => props.theme["gray-600"]};
+  }
+
+  &[data-state="checked"] {
+    background: ${(props) =>
+      props.variant === "income"
+        ? props.theme["green-500"]
+        : props.theme["red-500"]};
+    color: ${(props) => props.theme.white};
+
+    svg {
+      color: ${(props) => props.theme.white};
+    }
   }
 `;
